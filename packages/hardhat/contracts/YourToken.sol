945e2619ac8022cc91f15fead82c4fe4bf981ff6
Samuel Okpe <samokpe2@gmail.com>
2022-04-01 13:16:48 +0100

    Challenge-2

---
+++ packages/hardhat/contracts/YourToken.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+// Learn more about the ERC20 implementation 
+// on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/erc20
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YourToken is ERC20 {
+    constructor() ERC20("Scaffold ETH Token", "SET") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
